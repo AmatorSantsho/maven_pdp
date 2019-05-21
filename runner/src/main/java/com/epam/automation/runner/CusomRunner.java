ac9d1aec7c9b830f31b2ff6061b2308c8fc6937d
AmatorSantsho <ash002@tut.by>
2019-05-21 22:20:14 +0200

    Add properties to api_test module and runner

--- runner/src/main/java/com/epam/automation/runner/CusomRunner.java
+++ runner/src/main/java/com/epam/automation/runner/CusomRunner.java
@@ -1,42 +1,60 @@
 package com.epam.automation.runner;
 
-import org.junit.Test;
-import org.junit.runner.Description;
-import org.junit.runner.Runner;
-import org.junit.runner.notification.RunNotifier;
 
-import java.lang.reflect.Method;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 
-public class CusomRunner extends Runner {
-    private Class testClass;
-    public CusomRunner (Class testClass) {
-        super();
-        this.testClass = testClass;
-    }
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Iterator;
 
-    @Override
-    public Description getDescription() {
-        return Description
-                .createTestDescription(testClass, "My runner description");
-    }
+public class CusomRunner{
+    public static void main(String[] args) {
+        try
+        {
+            String fileName = args[0];
+            FileInputStream file = new FileInputStream(new File(fileName));
 
-    @Override
-    public void run(RunNotifier notifier) {System.out.println("running the tests from MyRunner: " + testClass);
-        try {
-            Object testObject = testClass.newInstance();
-            for (Method method : testClass.getMethods()) {
-                if (method.isAnnotationPresent(Test.class)) {
-                    notifier.fireTestStarted(Description
-                            .createTestDescription(testClass, method.getName()));
-                    method.invoke(testObject);
-                    notifier.fireTestFinished(Description
-                            .createTestDescription(testClass, method.getName()));
+            //Create Workbook instance holding reference to .xlsx file
+            HSSFWorkbook workbook = new HSSFWorkbook(file);
+
+            //Get first/desired sheet from the workbook
+            HSSFSheet sheet = workbook.getSheetAt(0);
+
+            //Iterate through each rows one by one
+            Iterator<Row> rowIterator = sheet.iterator();
+            while (rowIterator.hasNext())
+            {
+                Row row = rowIterator.next();
+                //For each row, iterate through all the columns
+                Iterator<Cell> cellIterator = row.cellIterator();
+
+                while (cellIterator.hasNext())
+                {
+                    Cell cell = cellIterator.next();
+                    //Check the cell type and format accordingly
+                    switch (cell.getCellType())
+                    {
+                        case Cell.CELL_TYPE_NUMERIC:
+                            System.out.print(cell.getNumericCellValue() + "t");
+                            break;
+                        case Cell.CELL_TYPE_STRING:
+                            System.out.print(cell.getStringCellValue() + "t");
+                            break;
+                    }
                 }
+                System.out.println("");
             }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+            file.close();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
         }
     }
 
-    }
+
+}
 
